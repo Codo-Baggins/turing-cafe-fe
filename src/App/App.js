6741ae343dd9da0a5ea840926c8be48773db52c3
Joe Lopez <lopez.4.joseph@gmail.com>
2020-12-16 09:23:37 -0700

    Build out state for app

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      allReservations: [],
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'>
-
-        </div>
-        <div className='resy-container'>
-          
-        </div>
+        <h1 className="app-title">Turing Cafe Reservations</h1>
+        <div className="resy-form"></div>
+        <div className="resy-container"></div>
       </div>
-    )
+    );
   }
 }
 
